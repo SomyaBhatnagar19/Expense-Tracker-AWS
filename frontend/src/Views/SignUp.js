46f648ae4776e1ebe85e340fe8e3b20829e8f1d9
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2024-01-23 12:21:39 +0530

    Task-3: Creating Login Screen Frontend.

--- frontend/src/Views/SignUp.js
+++ frontend/src/Views/SignUp.js
@@ -1,16 +1,23 @@
 /* /frontend/Views/SignUp.js */
 
-//it's a signup and signin file
-
 import React, { useState } from "react";
 
 export default function SignUp() {
   //state for showing signup form
   const [showSignUp, setShowSignUp] = useState(false);
+  //state for login form
+  const [showLogin, setLogin] = useState(true);
 
-  //function to set the state for shoing signup form
+  //function to set the state for showing signup form
   const SignUpButtonClicked = () => {
     setShowSignUp(true);
+    setLogin(false);
+  };
+
+  //function to set the state for showing login form
+  const LoginButtonClicked = () => {
+    setShowSignUp(false);
+    setLogin(true);
   };
 
   //states for input
@@ -47,52 +54,97 @@ export default function SignUp() {
 
   return (
     <div className="min-h-screen flex items-center justify-center">
-      {!showSignUp ? (
-        <div className="text-center bg-gradient-to-l from-cyan-600 to-cyan-700 text-white p-4 rounded-lg shadow-lg">
-          <h3 className="text-2xl font-semibold mb-2">Hello! New User?</h3>
-          <h5>Enter your details and start your journey.</h5>
-          <button
-            className="mt-2 text-white px-4 py-1 border rounded-xl"
-            onClick={SignUpButtonClicked}
-          >
-            SignUp
-          </button>
-        </div>
-      ) : (
-        <div className="bg-gradient-to-l from-slate-50 to-slate-100 text-center p-4 border rounded-lg shadow-lg">
-          <h3 className="text-2xl font-semibold mb-2">Create Account</h3>
-          <h5>Fill in your credentials.</h5>
-          <div className="mt-2 flex flex-col">
-            <input
-              className="border border-gray-300 rounded px-3 py-2 mt-2"
-              type="text"
-              placeholder="Name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-            <input
-              className="border border-gray-300 rounded px-3 py-2 mt-2"
-              type="email"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <input
-              className="border border-gray-300 rounded px-3 py-2 mt-2"
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
+      <div className="grid grid-cols-2 w-full max-w-md">
+        {/* Login */}
+        {!showSignUp || showLogin ? (
+          <div className="bg-gradient-to-l from-slate-50 to-slate-100 text-center p-4 border shadow-lg">
+            <h3 className="text-2xl font-semibold mb-2">Login</h3>
+            <h5>Use your credentials to login.</h5>
+            <form className="mt-2 flex flex-col">
+              <input
+                type="email"
+                placeholder="email"
+                className="border border-gray-300 rounded px-3 py-2 mt-2"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                type="password"
+                placeholder="password"
+                className="border border-gray-300 rounded px-3 py-2 mt-2"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <p>Forgot your password?</p>
+              <button
+                className="mt-1 text-white px-3 py-1 border rounded-xl bg-slate-800"
+                onClick={LoginButtonClicked}
+              >
+                LOGIN
+              </button>
+            </form>
+          </div>
+        ) : (
+          <div className="text-center bg-gradient-to-l from-cyan-600 to-cyan-700 text-white p-4 shadow-lg">
+            <h3 className="text-2xl font-semibold mb-2">Welcome Back!</h3>
+            <h5>Login with your details to stay connected.</h5>
             <button
-              className="mt-2 bg-cyan-900 hover:bg-cyan-700 focus:bg-cyan-700 text-white px-4 py-1 border rounded-xl"
-              onClick={handleSignUp}
+              className="mt-2 text-white px-4 py-1 border rounded-xl"
+              onClick={LoginButtonClicked}
+            >
+              Login
+            </button>
+          </div>
+        )}
+
+        {/* SignUp */}
+        {!showSignUp || showLogin ? (
+          <div className="text-center bg-gradient-to-l from-cyan-600 to-cyan-700 text-white p-4 shadow-lg">
+            <h3 className="text-2xl font-semibold mb-2">Hello! New User?</h3>
+            <h5>Enter your details and start your journey.</h5>
+            <button
+              className="mt-2 text-white px-4 py-1 border rounded-xl"
+              onClick={SignUpButtonClicked}
             >
               SignUp
             </button>
           </div>
-        </div>
-      )}
+        ) : (
+          <div className="bg-gradient-to-l from-slate-50 to-slate-100 text-center p-4 border shadow-lg">
+            <h3 className="text-2xl font-semibold mb-2">Create Account</h3>
+            <h5>Fill in your credentials.</h5>
+            <div className="mt-2 flex flex-col">
+              <input
+                className="border border-gray-300 rounded px-3 py-2 mt-2"
+                type="text"
+                placeholder="Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+              <input
+                className="border border-gray-300 rounded px-3 py-2 mt-2"
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                className="border border-gray-300 rounded px-3 py-2 mt-2"
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button
+                className="mt-2 bg-cyan-900 hover:bg-cyan-700 focus:bg-cyan-700 text-white px-4 py-1 border rounded-xl"
+                onClick={handleSignUp}
+              >
+                SignUp
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
